5bcdbdfb38d47fe1935c0bc8538d7651349215b2
Cesar <moraeschp@gmail.com>
2017-11-19 00:53:27 -0200

    Otimizei a função que define um inteiro primo

--- src/main.c
+++ src/main.c
@@ -1,17 +1,56 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+#include <unistd.h> // Permite a criacao de pipes
 
-int main() {
+/* Funcao que reconhece se um inteiro eh primo */
+int ehprimo(int numero){
+	int i, primo=1;
+	int flag=1; // Flag inicia em 1 (o divisor considerado se refere ao proprio numero n) para otimizar o codigo sem precisar percorrer todos os numeros de 1 a n, mas sim ate a metade
 
-  int x, y;
+	if(numero == 0 || numero == 1){
+		primo=0;
+		return primo;
+	}
+	if (numero == 2 || numero == 3){
+		primo=1;
+		return primo;
+	}
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	for(i=1; i<=(numero/2); i++){
+    		if(numero%i == 0){
+        		flag++;
+	
+			if(flag>2){
+				primo=0;
+				return primo;
+			}
+        	}
+	}
+
+return primo;
+}
+
+int main (){
+
+	int entrada, continua=1;
+	FILE *respostas;    
+    
+	respostas = popen("sort -n -r", "w"); // Abre um arquivo para escrita
+
+	while(continua){
+        
+        	scanf("%d\n", &entrada);
+        
+        	if(entrada == -1) // Indica o fim das entradas
+        		continua=0;
+
+        	else{
+        		if(ehprimo(entrada) == 0){ // Confere se a entrada eh um numero nao primo
+        			fprintf(respostas, "%d\n", entrada); // Escreve no arquivo
+			}
+		}
+	}
+
+pclose(respostas); // Fecha o arquivo
+
+return 0;
 }
